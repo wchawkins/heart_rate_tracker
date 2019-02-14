a8b0f1c40431eca16a102b8b85623dfcce788ed9
crooke <eric.crook@gmail.com>
2019-02-13 20:52:11 -0600

    Fix the main function to use the available_samples() method of the MAX class and print out if we lose any samples

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from converter import calculate_spo2_and_hr
 import max3010X
 import time
 
-
 def save_raw_data(red_buffer, ir_buffer):
     with open('red_ir.data', 'a') as file:
         for i in range(len(red_buffer)):
@@ -20,9 +19,13 @@ def main():
     while True:
         red_buffer = []
         ir_buffer = []
-        samples_to_read = SAMPLE_RATE/SAMPLE_AVG
 
-        for i in range(samples_to_read):
+        # Check if we lost samples b/c we are too slow
+        overflow_counter = m._read(0x05)[0]
+        if overflow_counter > 0:
+            print "Lost {} samples".format(overflow_counter)
+        
+        for i in range(m.available_samples()):
             red, ir = m.read_from_fifo()
             red_buffer.append(red)
             ir_buffer.append(ir)
@@ -30,12 +33,10 @@ def main():
         spo2, hr = calculate_spo2_and_hr(red_buffer, ir_buffer, SAMPLE_RATE, SAMPLE_AVG)
         # send_spo2_and_hr(spo2, hr)
         save_raw_data(red_buffer, ir_buffer)
-        time.sleep(0.04)
+
+        # Since the sensor is sampling at 25 Hz (samples/sec) and the FIFO
+        # can hold 32 samples, waiting 1 second b/w reads should be fine.
+        time.sleep(1)
 
 if __name__ == "__main__":
-    # main()
-    m = max3010X.MAX3010X()
-    m.setup(sample_rate=SAMPLE_RATE, sample_avg=SAMPLE_AVG)
-    print m.get_read_pointer()
-    print m.get_write_pointer()
-    print m.read_fifo_config()
+    main()
