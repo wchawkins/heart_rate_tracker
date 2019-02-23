fe9ec6a7d6356e889e79221de2056e7ac5815026
crooke <eric.crook@gmail.com>
2019-02-23 14:51:40 -0600

    Separated the graph.js from the main app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,5 +14,4 @@ import "phoenix_html"
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-import socket from "./socket"
-import graph from "./graph"
+// import socket from "./socket"
